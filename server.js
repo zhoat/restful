3612179cfd3c7868c3f6966cf18a3c8b5261c113
Zhoat <zhoatcat@gmail.com>
2016-03-08 23:27:24 +0800

    Add read function

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-var BodyParser = require( "body-parser" );
-var Http = require( "http" ),
-    Router = require( "router" ),
+var BodyParser = require( 'body-parser' );
+var Http = require( 'http' ),
+    Router = require( 'router' ),
     server,
     router;
 router = new Router();
@@ -14,12 +14,12 @@ server = Http.createServer( function( request, response ) {
       console.log( error.message, error.stack );
       response.writeHead( 400 );
     }
-    response.end( "RESTful API Server is running!" );
+    response.end( 'RESTful API Server is running!' );
   });
 });
 
 server.listen( 3000, function() {
-  console.log( "Listening on port 3000" );
+  console.log( 'Listening on port 3000' );
 });
 
 router.use( BodyParser.text() );
@@ -31,12 +31,33 @@ function createItem( request, response ){
     var id = counter += 1,
         item = request.body;
 
-    console.log( "Create item", id, item );
+    console.log( 'Create item', id, item );
     todoList[ id ] = item;
     response.writeHead(201, {
-        "Content-Type" : "text/plan",
-        "Location" : "/todo/" + id
+        'Content-Type' : 'text/plan',
+        'Location' : '/todo/' + id
     });
     response.end( item );
 }
-router.post( "/todo", createItem );
+
+function readItem( request, response ) {
+    var id = request.params.id,
+        item = todoList[ id ];
+ 
+    if ( typeof item !== 'string' ) {
+        console.log( 'Item not found', id );
+        response.writeHead( 404 );
+        response.end( '\n' );
+        return;
+    }
+ 
+    console.log( 'Read item', id, item);
+ 
+    response.writeHead( 200, {
+        'Content-Type' : 'text/plain'
+    });
+    response.end( item );
+}
+
+router.get( '/todo/:id', readItem );
+router.post( '/todo', createItem );
